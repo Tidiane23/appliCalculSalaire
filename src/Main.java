732f3a07d320d11c67ea324c600cb29785b27018
Tidiane DIALLO <tidianediallo426@gmail.com>
2024-08-28 22:54:03 +0200

    changement main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
         GestionEmployes gestion = new GestionEmployes();
 
-        gestion.ajouterEmploye(new Vendeur("Durand", "Pierre", 30, "2012", 50000));
+        gestion.ajouterEmploye(new Vendeur("Diallo", "Tidiane", 30, "2023", 33000));
+        gestion.ajouterEmploye(new Vendeur("Diallo", "Amadou", 31, "2024", 34000));
+        gestion.ajouterEmploye(new Vendeur("Durand", "Pierre", 29, "2012", 50000));
         gestion.ajouterEmploye(new Representant("Dupont", "Marie", 28, "2015", 60000));
         gestion.ajouterEmploye(new Producteur("Martin", "Jean", 40, "2010", 1000));
         gestion.ajouterEmploye(new Manutentionnaire("Bernard", "Luc", 45, "2008", 160));
